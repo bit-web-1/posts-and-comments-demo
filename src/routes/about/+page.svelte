eceff434de68e77dac0715e6ca7e4dcc664377a9
eallenop <elise.allen@op.ac.nz>
2025-10-08 15:22:54 +1300

    Initialise the about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    // Stuff here soon
+</script>
+
+<h2>About this site</h2>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga ad sequi molestiae tempora ducimus, dolor eius aliquid veritatis. Laudantium exercitationem, aut aspernatur sint nam expedita sit similique sapiente fugiat vero!</p>
