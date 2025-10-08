99cd47b5d0516e219681c41add600111d62384cc
eallenop <elise.allen@op.ac.nz>
2025-10-08 16:15:20 +1300

    Get the dynamic posts data from an external API

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,10 +1,13 @@
+<script>
+     let { data } = $props();
+</script>
+
 <h2>My Blog Contents Page</h2>
 
 <nav>
-    <a href="/blog/bananas">Bananas article</a>
-    <a href="/blog/web-is-cool">Web is Cool</a>
-    <a href="/blog/eight">Eight</a>
-    <a href="/blog/ideas-are-hard">Ideas are hard</a>
+    {#each data.posts as post}
+        <a href="/blog/{post.title}">{post.title}</a>
+    {/each}
 </nav>
 
 <style>
